b2626b2cae83dd1274c2286aa93a03efbb93dc7b
HurricaneLily <jostin27022023@gmail.com>
2025-02-19 13:51:07 +0400

    json-server
    Создаем БД новостей для эмуляции запросов к серверу

--- src/components/blocks/news/news.tsx
+++ src/components/blocks/news/news.tsx
@@ -1,6 +1,43 @@
+import { useEffect, useState } from "react";
 import NewsArticle from "../../ui/news-article/news-article";
 
+type Article = {
+  "title": string,
+  "description": string,
+  "content": string,
+  "url": string,
+  "image": string,
+  "publishedAt": string,
+  "source": {
+    "name": string,
+    "url": string
+  }
+}
+
+type ArticleList = {
+  "id": string,
+  "list" : Article[],
+}
+
 function News() {
+  const serverUrl = 'http://localhost:3000/articles';
+  const [articles, setArticles] = useState<ArticleList[]>([]);
+  const [category, setCategory] = useState("");
+
+  useEffect(() => {
+    fetch(serverUrl)
+      .then((resp) => resp.json())
+      .then((data: ArticleList[]) => {
+        if (category === "") {
+          setArticles(data);
+        } else {
+          const selectedData = data.filter((info: ArticleList) => info.id === category);
+          setArticles(selectedData);
+          console.log(selectedData);
+        }        
+      });
+  }, [category]);
+
   return (
     <>
       <h2>News</h2>
