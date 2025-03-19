a29f3374d0e84ef68ddacae7d3c359117293f2fa
HurricaneLily <jostin27022023@gmail.com>
2025-03-19 12:17:09 +0400

    Header (add logo)

--- src/layout/header/header.tsx
+++ src/layout/header/header.tsx
@@ -1,8 +1,12 @@
 import MainNav from "../../components/blocks/main-nav/main-nav";
-import { StyledHeader } from "./styles";
+import Logo from "../../assets/logo.png";
+import { StyledHeader, Link } from "./styles";
 
 function Header () {
     return <StyledHeader>
+        <Link to="/">
+            <img src={Logo} width={170} height={68} alt="Cool News"/>
+        </Link>
         <MainNav />
     </StyledHeader>;
 }
--- src/layout/header/styles.tsx
+++ src/layout/header/styles.tsx
@@ -1,7 +1,18 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const StyledHeader = styled.header`
+    width: 100%;
     max-width: 1700px;
     padding: 50px 0 20px;
     margin: 0 auto;
+    display: flex;
+    gap: 100px;
+    align-items: center;
+`
+
+export const Link = styled(NavLink)`
+    &:hover {
+        opacity: 0.8;
+    }
 `
